c3e63e6000e5857d883ee7f857aa7a185597881c
be <biabone07@gmail.com>
2024-10-17 10:12:51 -0300

    Alteração tal de tal

--- projeto.py
+++ projeto.py
@@ -1,11 +1,20 @@
 class Produto:
     def _init_ (self, produto, preço, marca, estoque_disponivel):
-        produtos = []
+        self.produtos = []
 
-class Cliente
+    def adicionar_produto(self, produto):
+        self.produtos.append (produto)
+        
+    
+class Cliente:
     def _init_(self, nome, telefone, historico_de_compras, produto, preço, marca, estoque_disponivel):
         super()._init_(produto, preço, marca, estoque_disponivel)
-    clientes = []
+        self.clientes = []
 
-class Compra 
+    def adicionar_cliente(self, cliente):
+        self.clientes.append (cliente)
+        
+    def total_compra(self,):
+
+class Compra:
     def _init_(self, data_compra, cliente_associado, produtos_comprados, status):
