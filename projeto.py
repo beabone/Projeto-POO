3b407e67fc4431f912f2e919eca7935cd58505ab
be <biabone07@gmail.com>
2024-10-17 09:37:39 -0300

    oie

---
+++ projeto.py
@@ -0,0 +1,11 @@
+class Produto:
+    def _init_ (self, produto, preço, marca, estoque_disponivel):
+        produtos = []
+
+class Cliente
+    def _init_(self, nome, telefone, historico_de_compras, produto, preço, marca, estoque_disponivel):
+        super()._init_(produto, preço, marca, estoque_disponivel)
+    clientes = []
+
+class Compra 
+    def _init_(self, data_compra, cliente_associado, produtos_comprados, status):
