d6832e44c4ae1a6de28159b3d84108958adc6cbf
be <biabone07@gmail.com>
2024-10-21 14:56:14 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -1,20 +1,29 @@
 class Produto:
     def _init_ (self, produto, preço, marca, estoque_disponivel):
-        self.produtos = []
-
-    def adicionar_produto(self, produto):
-        self.produtos.append (produto)
-        
+        self.nome = nome
+        self.preco = preço
+        self.categoria = marca
+        self.estoque_disponivel = estoque_disponivel
     
 class Cliente:
     def _init_(self, nome, telefone, historico_de_compras, produto, preço, marca, estoque_disponivel):
         super()._init_(produto, preço, marca, estoque_disponivel)
-        self.clientes = []
-
-    def adicionar_cliente(self, cliente):
-        self.clientes.append (cliente)
+        self.nome = nome
+        self.telefone = telefone
+        self.historico_de_compras = []
+    
+    def adicionar_compra(self, compra):
+        self.historico_de_compras.append(compra)
         
-    def total_compra(self,preço,):
+    def total_compras (self):
+        return sum(compra.calcular_total() for compra in self.historico_de_compras)
 
 class Compra:
     def _init_(self, data_compra, cliente_associado, produtos_comprados, status):
+        self.data_compra = data_compra
+        self.cliente_associado = cliente_associado
+        self.produtos_comprados = produtos_comprados
+        self.status = status
+    
+    def calcular_total(self):
+        return sum(produto.preço for produto in self.produtos_comprados)
